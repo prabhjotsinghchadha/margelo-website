0dda82a72232e47245c9ad117f50b4c9a451a924
Prabhjot <prabhjottechs@gmail.com>
2022-05-06 13:13:17 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: [
+        './pages/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',
+    ],
+    theme: {
+        letterSpacing: {
+            tight: '-.015em'
+        },
+        extend: {
+            height: {
+                'half-screen': '50vh'
+            }
+        }
+    },
+    plugins: []
+}
